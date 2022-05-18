31a1434d60a42eef1ccf96ae45ebe00491aaffcd
Paulo Filho <paulopbalsamo@gmail.com>
2022-05-18 08:01:33 -0300

    adicionando acesso BD

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,18 +1,18 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
+	
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
 
 	public List<Department> findAll() {
-		List<Department> list = new ArrayList<Department>();
-		list.add(new Department(1, "Loja 1"));
-		list.add(new Department(2, "Loja 2"));
-		list.add(new Department(3, "Loja 3"));
-		return list;
+	return dao.findAll();
 
 	}
 
